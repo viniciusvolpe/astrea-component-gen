b385902765fa9d0968edbfc384dc4afa38ed121b
viniciusvolpesousa <vinicius.souza@aurum.com.br>
2017-12-13 11:08:15 -0200

    Adding option to short names

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ prog
     .version('1.0.0')
     .command('create', 'Create a new angular component')
     .argument('<name>', 'Component name')
+    .option('--short', 'Short files names')
     .action((args, options, logger) => {
         const name = args.name
         shell.mkdir(name)
         shell.cd(name)
-        shell.cp(`${templatePath}/component/component.js`, `${localPath}/${name}/${name}.component.js`)
-        shell.cp(`${templatePath}/controller/controller.js`, `${localPath}/${name}/${name}.controller.js`)
-        shell.cp(`${templatePath}/test/test.js`, `${localPath}/${name}/${name}.controller.spec.js`)
-        shell.touch(`${name}.html`)
-        shell.touch(`${name}.style.css`)
+        if(options.short)
+            createFilesWithShortNames(name)
+        else
+            createFilesWithFullNames(name)
         shell.ls('*.js').forEach(function (file) {
             shell.sed('-i', '{name}', name, file);
             shell.sed('-i', '{nameVar}', camelCase(name), file);
@@ -27,3 +27,19 @@ prog
     });
 
 prog.parse(process.argv);
+
+function createFilesWithFullNames(name){
+    shell.cp(`${templatePath}/component/component.js`, `${localPath}/${name}/${name}.component.js`)
+    shell.cp(`${templatePath}/controller/controller.js`, `${localPath}/${name}/${name}.controller.js`)
+    shell.cp(`${templatePath}/test/test.js`, `${localPath}/${name}/${name}.controller.spec.js`)
+    shell.touch(`${name}.html`)
+    shell.touch(`${name}.style.css`)
+}
+
+function createFilesWithShortNames(name){
+    shell.cp(`${templatePath}/component/component.js`, `${localPath}/${name}/component.js`)
+    shell.cp(`${templatePath}/controller/controller.js`, `${localPath}/${name}/controller.js`)
+    shell.cp(`${templatePath}/test/test.js`, `${localPath}/${name}/test.js`)
+    shell.touch(`template.html`)
+    shell.touch(`style.css`)
+}
